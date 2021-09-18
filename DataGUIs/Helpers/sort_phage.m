ef1aef51618103d7e989ef7d6661e32c133bb406
abmtong <abmtong@gmail.com>
2021-09-17 18:32:35 -0700

    token update

--- DataGUIs/Helpers/sort_phage.m
+++ DataGUIs/Helpers/sort_phage.m
@@ -14,7 +14,7 @@ for i = 1:length(infilenames)
             mm = (str(st:st+1));
             dd = (str(st+2:st+3));
             yy = (str(st+4:st+5));
-            sortnum(j) = daysact('1/1/15',[mm '/' dd '/' yy]);
+            sortnum(j) = daysact('1/1/15',[mm '/' dd '/' yy]); %Financial toolbox only. Find a replacement, eg round(posixtime( datetime ( 'now' ) )/3600/24-45*365)
         elseif en(j)-st(j) <5 %5 char or less, should be sortable
             sortnum(j) = str2double(str(st(j):en(j)))+127; %The last 'regular' character is tilde, 126, so put numbers after a-z
         else %number is larger than 6 char.
