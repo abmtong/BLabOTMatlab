fbae8df404e52bcc4078a1b5dae7598668889f5b
abmtong <abmtong@gmail.com>
2022-03-02 10:20:12 -0800

    Deleted some files

--- Misc/Game-Related/stardewfair/spin.m
+++
@@ -1,8 +0,0 @@
-function out = spin(ncoin, ratio, nspin)
-
-out = [ncoin zeros(1, nspin)];
-
-%stardew valley wheel is 75% winrate to double, what is the optimal betting strategy?
-for i = 1:nspin
-    out(i+1) = out(i) + round(out(i)*ratio) * (2*floor(rand+.75)-1);
-end
